2342686e56c0083df6273c23d08b306c7f36c88d
Bratislav Stojanovic <bata.stojan.83@gmail.com>
2020-02-14 16:47:12 +0100

    Updates

---
+++ precache-manifest.92427ba66cd2475c03061ce4237a9eef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e802a54764b359358fcdb858cc13b7a",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "914b111081bc94393ae5",
+    "url": "/react-todo/static/css/main.71f2fce9.chunk.css"
+  },
+  {
+    "revision": "6a6cb1235700bdada0ab",
+    "url": "/react-todo/static/js/2.e724de16.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-todo/static/js/2.e724de16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "914b111081bc94393ae5",
+    "url": "/react-todo/static/js/main.8c524008.chunk.js"
+  },
+  {
+    "revision": "4637a77acbbb1bfa3441",
+    "url": "/react-todo/static/js/runtime-main.852ef8c9.js"
+  }
+]);
